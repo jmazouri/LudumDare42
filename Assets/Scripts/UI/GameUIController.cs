753bc0d56e80fad0503796e96b1f7b5cbb934124
Matt Jones <matthewj866@gmail.com>
2018-08-11 23:30:14 +0200

    Added DARTIS Dialogue System.

--- Assets/Scripts/UI/GameUIController.cs
+++ Assets/Scripts/UI/GameUIController.cs
@@ -1,4 +1,6 @@
-﻿using TMPro;
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,19 +13,56 @@ public class GameUIController : MonoBehaviour
     [SerializeField] private int _maxHealth;
     [SerializeField] private int _maxAmmo;
     [SerializeField] private bool _demo;
+    [SerializeField] private GameObject _dialogueObject;
+    [SerializeField] private TextMeshProUGUI _dialogueTextBox;
+    [SerializeField] private int _characterLimit;
+    [SerializeField] private float _maxReadingTime;
+    
+    private List<string> _dialogues;
+    private float _readingTimePassed;
 
     private void Start()
     {
+        _dialogues = new List<string>(10);
+        _dialogueTextBox.text = string.Empty;
+        _dialogueObject.SetActive(false);
         _healthBar.maxValue = _maxHealth;
         _ammoBar.maxValue = _maxAmmo;
         AssignNewHealth(_maxHealth);
         AssignNewAmmo(_maxAmmo);
-        
+
         if (!_demo) return;
-        
+
         AssignNewHealth(_maxHealth / 2);
         AssignNewAmmo(_maxAmmo / 2);
         AssignNewScore(5050);
+        QueueNewDialogueText("WEW LAD");
+        QueueNewDialogueText("ITS WORKING");
+        QueueNewDialogueText("Notice me player-kun.");
+    }
+
+    void Update()
+    {
+        if (_dialogues.Count > 0 && !_dialogueObject.active)
+        {
+            _dialogueObject.SetActive(true);
+        }
+        else if (_dialogueObject.active && _dialogues.Count == 0 && _readingTimePassed > _maxReadingTime)
+        {
+            _dialogueObject.SetActive(false);
+        }
+
+        if (_dialogueObject.activeInHierarchy && (_readingTimePassed >= _maxReadingTime || _dialogueTextBox.text == string.Empty))
+        {
+            var text = _dialogues[0];
+            _dialogueTextBox.text = text;
+            _dialogues.Remove(text);
+            _readingTimePassed = 0f;
+        }
+        else if (_dialogueObject.activeInHierarchy)
+        {
+            _readingTimePassed += Time.deltaTime;
+        }
     }
 
     public void AssignNewHealth(float health)
@@ -45,5 +84,16 @@ public class GameUIController : MonoBehaviour
         var result = Mathf.Clamp(score, 0, 9999999999999).ToString("0000000000000");
         _score.text = result;
         _shadowScore.text = result;
+    }
+
+    public void QueueNewDialogueText(string text)
+    {
+        if (text.Length > _characterLimit)
+        {
+            throw new FormatException(
+                "String exceeds the character limit of the Dialogue box. Please split the string and parse them in one at a time.");
+        }
+
+        _dialogues.Add(text);
     }
 }
