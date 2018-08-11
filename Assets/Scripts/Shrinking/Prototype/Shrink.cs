99808a26984d4ca29a2980d9fd05299f1dea1cb6
Kirill <Arkrait04@gmail.com>
2018-08-11 14:34:43 +0300

    awful shrinking

---
+++ Assets/Scripts/Shrinking/Prototype/Shrink.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LD42.Shrinking.Prototypes
+{
+    public class Shrink : MonoBehaviour
+    {
+        [SerializeField] private float _shrinkAmount;
+        private float _resultScaleX;
+        private float _resultScaleY;
+        
+        void Start ()
+        {
+            _resultScaleX = transform.localScale.x / _shrinkAmount;
+            _resultScaleY = transform.localScale.y / _shrinkAmount;
+            var resultScale = new Vector3(_resultScaleX, _resultScaleY);
+            transform.localScale = resultScale;
+        }
+    }
+}
