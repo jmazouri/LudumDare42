25ded091df84828edf4278676845ed42d5775cf7
Patrick Klaeren <patrick@totalren.com>
2018-08-11 10:53:35 +0100

    Probably bad movement
    Learning how to do movement in Unity - probably broke every rule in the book

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    public float Speed;
+
+    private Rigidbody2D body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        var horizontal = Input.GetAxis ("Horizontal");
+        var vertical = Input.GetAxis ("Vertical");
+
+        var movement = new Vector2 (horizontal, vertical);
+
+        body.AddForce(movement * Speed);
+    }
+}
