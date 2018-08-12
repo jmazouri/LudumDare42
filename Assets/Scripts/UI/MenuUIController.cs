85f08004a98088dd607533c7ded2a141e41d846f
Matt Jones <matthewj866@gmail.com>
2018-08-12 19:29:52 +0200

    Updated ALL THE UI!

---
+++ Assets/Scripts/UI/MenuUIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIController : MonoBehaviour
+{
+    [SerializeField] private GameObject _controlsMenu;
+    [SerializeField] private GameObject _mainMenu;
+    
+    public void ShowControlsMenu()
+    {
+        _mainMenu.SetActive(false);
+        _controlsMenu.SetActive(true);
+    }
+
+    public void ShowMainMenu()
+    {
+        _mainMenu.SetActive(true);
+        _controlsMenu.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
